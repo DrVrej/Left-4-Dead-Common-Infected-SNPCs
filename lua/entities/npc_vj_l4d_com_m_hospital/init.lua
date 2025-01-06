91004fdec0352216eb3d4984740a29cd75dcc8eb
DrVrej <DrVrej@users.noreply.github.com>
2025-01-06 00:07:49 -0500

    Revamp tags + Update CR date

--- lua/entities/npc_vj_l4d_com_m_hospital/init.lua
+++ lua/entities/npc_vj_l4d_com_m_hospital/init.lua
@@ -2,7 +2,7 @@ include("vj_base/extensions/l4d_com_infected.lua")
 AddCSLuaFile("shared.lua")
 include("shared.lua")
 /*-----------------------------------------------
-	*** Copyright (c) 2012-2024 by DrVrej, All rights reserved. ***
+	*** Copyright (c) 2012-2025 by DrVrej, All rights reserved. ***
 	No parts of this code or any of its contents may be reproduced, copied, modified or adapted,
 	without the prior written consent of the author, unless otherwise indicated for stand-alone materials.
 -----------------------------------------------*/
