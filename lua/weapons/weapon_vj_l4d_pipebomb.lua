afcb2263ad6cdf86daff2efa1f8d6ba4788dad03
DrVrej <DrVrej@users.noreply.github.com>
2026-03-28 08:53:18 +0500

    Pipe Bomb weapon updates + general cleanups

--- lua/weapons/weapon_vj_l4d_pipebomb.lua
+++ lua/weapons/weapon_vj_l4d_pipebomb.lua
@@ -1,57 +1,43 @@
+AddCSLuaFile()
+
 SWEP.Base = "weapon_vj_base"
 SWEP.PrintName = "Pipe Bomb"
 SWEP.Author = "DrVrej"
 SWEP.Contact = "http://steamcommunity.com/groups/vrejgaming"
 SWEP.Category = "Left 4 Dead"
-	-- Client Settings ---------------------------------------------------------------------------------------------------------------------------------------------
-if CLIENT then
-	SWEP.Slot = 4
-	SWEP.SlotPos = 4
-	SWEP.SwayScale = 2
-	SWEP.ViewModelFOV = 90
-	SWEP.UseHands = true
-end
-	-- NPC Settings ---------------------------------------------------------------------------------------------------------------------------------------------
-SWEP.NPC_NextPrimaryFire = 10
-	-- Main Settings ---------------------------------------------------------------------------------------------------------------------------------------------
+SWEP.Spawnable = true
+SWEP.AdminOnly = true
+
 SWEP.ViewModel = "models/cpthazama/l4d1/weapons/v_pipebomb.mdl"
 SWEP.WorldModel = "models/cpthazama/l4d1/weapons/pipebomb.mdl"
 SWEP.HoldType = "grenade"
-SWEP.Spawnable = true
-SWEP.AdminOnly = true
-	-- Primary Fire ---------------------------------------------------------------------------------------------------------------------------------------------
-SWEP.Primary.Damage = 0
-SWEP.Primary.Force = 1
+SWEP.WorldModel_UseCustomPosition = true
+SWEP.WorldModel_CustomPositionAngle = Vector(0, 0, 180)
+SWEP.WorldModel_CustomPositionOrigin = Vector(-1.5, 3, 2)
+SWEP.ViewModelFOV = 90
+SWEP.Slot = 4
+SWEP.SlotPos = 4
+SWEP.SwayScale = 2
+SWEP.UseHands = true
+
+SWEP.NPC_NextPrimaryFire = 10
+
+SWEP.Primary.DisableBulletCode = true
 SWEP.Primary.ClipSize = 1
 SWEP.Primary.Recoil = 2
 SWEP.Primary.Delay = 10
 SWEP.Primary.Automatic = false
 SWEP.Primary.Ammo = "pipebomb"
-SWEP.Primary.Sound = {}
-SWEP.Primary.DistantSound = {}
 SWEP.AnimTbl_PrimaryFire = ACT_VM_PRIMARYATTACK
-//SWEP.Primary.DistantSoundVolume	= 0.25
-SWEP.Primary.DisableBulletCode = true
 SWEP.PrimaryEffects_MuzzleFlash = false
 SWEP.PrimaryEffects_SpawnDynamicLight = false
 SWEP.PrimaryEffects_SpawnShells = false
-SWEP.Primary.TakeAmmo = 1
-	-- Deployment Settings ---------------------------------------------------------------------------------------------------------------------------------------------
-SWEP.DelayOnDeploy = 0.35
-	-- Idle Settings ---------------------------------------------------------------------------------------------------------------------------------------------
-SWEP.HasIdleAnimation = true -- Does it have a idle animation?
-SWEP.AnimTbl_Idle = ACT_VM_IDLE
-SWEP.NextIdle_Deploy = 0.35 -- How much time until it plays the idle animation after the weapon gets deployed
-SWEP.NextIdle_PrimaryAttack = 1.3 -- How much time until it plays the idle animation after attacking(Primary)
-	-- Reload Settings ---------------------------------------------------------------------------------------------------------------------------------------------
+
 SWEP.AnimTbl_Reload = ACT_VM_DRAW
 SWEP.Reload_TimeUntilAmmoIsSet = 1
-	-- World Model ---------------------------------------------------------------------------------------------------------------------------------------------
-SWEP.WorldModel_UseCustomPosition = true
-SWEP.WorldModel_CustomPositionAngle = Vector(0, 0, 180)
-SWEP.WorldModel_CustomPositionOrigin = Vector(-1.5, 3, 2)
 
-SWEP.ViewModelAdjust = {
+-- Custom
+SWEP.L4D_ViewModelAdjust = {
 	Pos = {Right = 0, Forward = 0, Up = -6},
 	Ang = {Right = 0, Up = 0, Forward = 0}
 }
@@ -79,7 +65,7 @@ function SWEP:OnPrimaryAttack(status, statusData)
 				pipeBomb:Spawn()
 
 				local phys = pipeBomb:GetPhysicsObject()
-				if phys:IsValid() then
+				if IsValid(phys) then
 					phys:ApplyForceCenter(owner:GetAimVector()*5000 + owner:GetUp()*1500)
 					phys:SetAngleVelocity(Vector(math.Rand(-160, 160), math.Rand(-160, 160), math.Rand(-160, 160)))
 				end
@@ -90,7 +76,7 @@ end
 ---------------------------------------------------------------------------------------------------------------------------------------------
 if CLIENT then
 	function SWEP:GetViewModelPosition(pos, ang)
-		local vModelAdjust = self.ViewModelAdjust
+		local vModelAdjust = self.L4D_ViewModelAdjust
 		pos:Add(ang:Right() * vModelAdjust.Pos.Right)
 		pos:Add(ang:Forward() * vModelAdjust.Pos.Forward)
 		pos:Add(ang:Up() * vModelAdjust.Pos.Up)
